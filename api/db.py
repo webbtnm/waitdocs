95474bebc68e3752b839c23047cba0af2a64d98d
vlavrehin <lavrehinvl@yandex.ru>
2024-12-25 21:24:41 +0300

    docker and folder structure

--- api/db.py
+++ api/db.py
@@ -1,9 +1,12 @@
-
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
+import os
 
-DATABASE_URL = "postgresql://user:password@localhost:5432/dbname"
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
